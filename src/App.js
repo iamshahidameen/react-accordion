2348eb0b6008b254d6628bc873f27bdbf5525872
Shahid Ameen <43674745+iamshahidameen@users.noreply.github.com>
2022-07-20 21:06:12 +0500

    Added Project Starter Files

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import data from './data';
+import Question from './Question';
+import SingleQuestion from './Question';
+function App() {
+  return <></>;
+}
+
+export default App;
---
+++ src/Question.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+const Question = () => {
+  return <></>;
+};
+
+export default Question;
