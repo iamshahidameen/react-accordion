878f46bc68c9998de15272ec2d8f5badd21de1c9
Shahid Ameen <43674745+iamshahidameen@users.noreply.github.com>
2022-07-20 21:06:35 +0500

    Added a simple layout for accordions

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import data from './data';
 import Question from './Question';
 import SingleQuestion from './Question';
 function App() {
-  return <></>;
+  return (
+    <>
+      <main>
+        <div className="container">
+          <h3>questions and answers about login</h3>
+          <section className="info">
+            <Question />
+          </section>
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default App;
--- src/Question.js
+++ src/Question.js
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 const Question = () => {
-  return <></>;
+  return (
+    <>
+      <article className="question">
+        <header>
+          <h4>Do I have to allow the use of cookes?</h4>
+          <button className="btn">
+            <AiOutlineMinus />
+          </button>
+        </header>
+        <p>
+          Unicorn vinyl poutine brooklyn, next level direct trade iceland.
+          Shaman copper mug church-key coloring book, whatever poutine normcore
+          fixie cred kickstarter post-ironic street art.
+        </p>
+      </article>
+    </>
+  );
 };
 
 export default Question;
